475c975d9c2de1ee50d4cb712a7797edbb53a60f
Tokxhu <thomas.granbohm@hotmail.com>
2020-01-31 11:09:36 +0100

    fixed bugs

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ client.on('message', async (message) => {
 
 	try {
 		let toSend = await command.run(message, args.join(" "));
-		utils.sendMessage.run(
+		client.utils.sendMessage.run(
 			channel,
 			toSend,
 			command.category,
 			author
 		);
 	} catch (error) {
-		await utils.sendError.run(message, error)
+		await client.utils.sendError.run(message, error)
 	}
 })
 
@@ -110,14 +110,14 @@ client.on('ready', async () => {
 		}
 	});
 
-	new CronJob('0 30 10 * * 1-5', async () => {
+//	new CronJob('0 30 10 * * 1-5', async () => {
 		let channels = await client.models.channels.findAll()
 		let lunch = await client.utils.lunchEmbed.run();
 		channels.forEach(dbChannel => {
 			let channel = client.channels.get(dbChannel.channelID);
 			client.utils.sendMessage.run(channel, lunch, config.categories.MISC);
 		})
-	}).start();
+//	}).start();
 
 	client.user.setActivity('bass boosted music!', { type: 'LISTENING' })
 
