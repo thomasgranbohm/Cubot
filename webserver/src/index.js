2c44d30f01d0d187b76470f5f0b37bc8397db4e2
Tokxhu <thomas.granbohm@hotmail.com>
2020-08-23 21:51:19 +0200

    Removed service-worked from index.js

--- webserver/src/index.js
+++ webserver/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 let { NODE_ENV, PUBLIC_URL } = process.env;
@@ -14,5 +13,3 @@ ReactDOM.render(
 	</BrowserRouter>,
 	document.getElementById('root')
 );
-
-serviceWorker.unregister();
