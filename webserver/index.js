ecf2ea3e641bf2e95cda64c568e26d3e580bf25f
Tokxhu <thomas.granbohm@hotmail.com>
2020-09-01 14:39:54 +0200

    ☁️ Added link to GitHub repo

--- webserver/index.js
+++ webserver/index.js
@@ -9,88 +9,82 @@ const logger = require('../logger/');
 const lastParam = process.argv.pop();
 
 if (lastParam === "no-logger")
-	app.use(morgan('tiny'));
+    app.use(morgan('tiny'));
 
 app.use(cors());
 
 const categories = Object.entries(require('../config.json').categories)
-	.map(([key, value]) => {
-		return {
-			// name: key.substr(0, 1).toUpperCase() + key.substr(1).toLowerCase(),
-			name: key,
-			color: value
-		}
-	});
+    .map(([key, value]) => {
+        return {
+            // name: key.substr(0, 1).toUpperCase() + key.substr(1).toLowerCase(),
+            name: key,
+            color: value
+        }
+    });
 
 const { WEBSERVER_PORT } = process.env;
 
 app.start = () => {
-	let bot = require('../bot');
+    let bot = require('../bot');
 
-	const api = express.Router();
+    const api = express.Router();
 
-	app.use('/public', express.static(path.join(__dirname, 'public')))
+    app.use('/public', express.static(path.join(__dirname, 'public')))
 
-	app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'build')));
 
-	app.use('/api', api);
+    app.use('/api', api);
 
-	app.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'));
-	});
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
-	api.get('/', (req, res) => {
-		let info = {
-			guilds: bot.guilds.cache.size,
-			members: bot.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0),
-			players: bot.manager.players.size
-		};
+    api.get('/', (req, res) => {
+        let info = {
+            guilds: bot.guilds.cache.size,
+            members: bot.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0),
+            players: bot.manager.players.size
+        };
 
-		res.send(info);
-	})
+        res.send(info);
+    })
 
-	api.get('/commands', (req, res) => {
-		let commands = Object.entries(bot.commands).map(([key, value]) => {
-			return {
-				...value,
-				name: key,
-				category: categories.find(cat => cat.color == value.category)
-			}
-		})
-		res.send({
-			commands,
-			categories
-		})
-	})
+    api.get('/commands', (req, res) => {
+        let commands = Object.entries(bot.commands).map(([key, value]) => {
+            return {
+                ...value,
+                name: key,
+                category: categories.find(cat => cat.color == value.category)
+            }
+        })
+        res.send({
+            commands,
+            categories
+        })
+    })
 
-	api.get('/categories', (req, res) => res.send(categories))
+    api.get('/categories', (req, res) => res.send(categories))
 
-	api.get('/guilds', async (req, res) => {
-		let guilds = bot.guilds.cache
-			.map(async (guild) => {
-				let {
-					channels,
-					id,
-					members,
-					name,
-					ownerID,
-				} = guild;
-				return {
-					icon: guild.iconURL(),
-					channels: channels.cache.size,
-					id,
-					members: members.cache.size,
-					name,
-					owner: await bot.users.fetch(ownerID),
-				};
-			})
-			.sort((a, b) => a.name >= b.name)
-		res.send(await Promise.all(guilds));
-	})
+    api.get('/guilds', async (req, res) => {
+        let guilds = bot.guilds.cache
+            .map(async (guild) => {
+                let { channels, id, members, name, ownerID } = guild;
+                return {
+                    icon: guild.iconURL(),
+                    channels: channels.cache.size,
+                    id,
+                    members: members.cache.size,
+                    name,
+                    owner: await bot.users.fetch(ownerID),
+                };
+            })
+            .sort((a, b) => a.name >= b.name)
+        res.send(await Promise.all(guilds));
+    })
 
-	app.listen(WEBSERVER_PORT, () => {
-		logger.log(`Listening on ::${WEBSERVER_PORT}`);
-	})
+    app.listen(WEBSERVER_PORT, () => {
+        logger.log(`Listening on ::${WEBSERVER_PORT}`);
+    })
 }
 
 module.exports = app;
