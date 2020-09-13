3e999d330c1ca87e821f5af86908ed1e92050ca6
Tokxhu <thomas.granbohm@hotmail.com>
2020-09-13 14:50:27 +0200

    🔥 Now using Google hosted thumbnails

--- src/utils/getThumbnail.ts
+++ src/utils/getThumbnail.ts
@@ -1,62 +1,15 @@
 import { TrackObject } from "../types";
-import { resolve } from "path";
 import axios from "axios";
-import { promises as fs, writeFileSync, createWriteStream } from 'fs';
-import sharp from "sharp";
-
-let download = function (uri: string, path: string, filename: string): Promise<string> {
-	return new Promise(async (res, rej) => {
-		try {
-			await fs.mkdir(path);
-		} catch (err) { }
-
-		let fullPath = resolve(path, filename);
-		try {
-			let resp = await axios.get(uri, {
-				responseType: "stream"
-			});
-			let filestream = await createWriteStream(fullPath)
-				.on('finish', () => {
-					filestream.end();
-					res(fullPath)
-				})
-
-			resp.data.pipe(filestream);
-		} catch (error) {
-			rej(error)
-		}
-	});
-};
 
 export default async function (track: TrackObject): Promise<string | null> {
-	if (!track.uri.includes("youtube") || typeof track.thumbnail === "string")
+	if (!track.uri.includes("youtube") || typeof track.thumbnail === "string") {
 		return null;
-	let resolutions = [
-		'maxres',
-		'sd',
-		'hq',
-		'mq',
-	];
+	}
 
-	for (let res of resolutions) {
-		let path = resolve(process.env.PWD || __dirname, `./thumbnails/`);
+	for (let res of ['maxres', 'mq']) {
 		try {
-			let fullPath = await download(
-				`https://i.ytimg.com/vi/${track.identifier}/${res}default.jpg`,
-				path,
-				`${track.identifier}-${res}.jpg`
-			);
-
-			let buffer = await sharp(fullPath)
-				.resize({
-					width: 1600,
-					height: 900,
-					fit: "cover"
-				})
-				.jpeg()
-				.toBuffer();
-
-			await writeFileSync(fullPath, buffer, { flag: "w" });
+			let fullPath = `https://i.ytimg.com/vi_webp/${track.identifier}/${res}default.webp`;
+			await axios.get(fullPath);
 			return fullPath;
 		} catch (err) {
 			if (!err.isAxiosError)
