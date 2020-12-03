cdbefb6f8d50657c677a884b19d1e716936cc6dd
Thomas Granbohm <thomas.granbohm@gmail.com>
2020-12-03 15:15:35 +0100

    🎨 Removed unnecessary functions and improved structure of index.ts

--- src/index.ts
+++ src/index.ts
@@ -61,131 +61,130 @@ export class Bot extends Client {
 		this.login(token);
 	}
 
-	async connectToLavalink(retryIndex = 0) {
-		try {
-			await this.manager.connect();
-			console.log('Successfully connected to Lavalink.');
-		} catch (err) {
-			if (retryIndex === 2) {
-				console.error(
-					`Could not connect to Lavalink on ${LavalinkConfig.host}:${LavalinkConfig.port}! Exiting...`
-				);
-				process.exit(1);
+	private async onReady() {
+		const loadCommands = () => {
+			const entries = Object.entries(commands);
+			for (const [name, TempCommand] of entries) {
+				this.commands.set(name.toLowerCase(), new TempCommand(this));
 			}
-			console.log(`#${++retryIndex}. Retrying Lavalink connection...`);
-			setTimeout(() => this.connectToLavalink(retryIndex), 5000);
-		}
-	}
+		};
 
-	loadCommands() {
-		const entries = Object.entries(commands);
-		for (const [name, TempCommand] of entries) {
-			this.commands.set(name.toLowerCase(), new TempCommand(this));
-		}
-	}
+		const connectToLavalink = async (retryIndex = 0) => {
+			try {
+				await this.manager.connect();
+				console.log('Successfully connected to Lavalink.');
+			} catch (err) {
+				if (retryIndex === 2) {
+					console.error(
+						`Could not connect to Lavalink on ${LavalinkConfig.host}:${LavalinkConfig.port}! Exiting...`
+					);
+					process.exit(1);
+				}
+				console.log(
+					`#${++retryIndex}. Retrying Lavalink connection...`
+				);
+				setTimeout(() => connectToLavalink(retryIndex), 5000);
+			}
+		};
 
-	async handleCommand(message: Message) {
-		const { id: messageId, content, author, mentions, channel } = message;
-
-		const isBot = author.bot;
-		if (isBot) return;
-
-		if (channel instanceof DMChannel) return;
-
-		const guild = await getGuildFromMessage(message);
-		const prefix = await this.guildResolver.prefix(guild.id);
-
-		const hasPrefix =
-			content.startsWith(prefix) ||
-			content.startsWith(prefix.concat(' '));
-		const mentionsBot = this.user
-			? mentions.has(this.user, {
-					ignoreEveryone: true,
-					ignoreRoles: true,
-			  })
-			: false;
-
-		if (!hasPrefix && !mentionsBot) return;
-
-		checkPermissions(guild);
-
-		const { id: guildId } = guild;
-
-		const prefixLength =
-			prefix.length + +content.startsWith(prefix.concat(' '));
-		const [name, ...args] = content.substr(prefixLength).split(' ');
-
-		const command = this.commands.find((c) =>
-			c.names.includes(mentionsBot ? 'help' : name)
-		);
-		if (!command) return;
-
-		if (command.needsArgs && args.length === 0)
-			throw new ArgumentError(command, prefix);
-
-		if (command.ownerOnly && author.id !== this.owner)
-			throw new OwnerError();
-
-		if (command.guildOnly && channel instanceof TextChannel === false)
-			throw new GuildOnlyError();
-
-		// TODO this do be kinda ugly tho
-		addToCommandQueue(guildId, messageId, {
-			channel,
-			command,
-			options: {
-				message,
-				args,
-				category: command.group,
-			},
-		});
-	}
-
-	// MEH: Message Error Handling
-	handleError(message: Message, error: Error) {
-		if (
-			error instanceof CustomError === false &&
-			error instanceof DiscordAPIError === false
-		)
-			return console.error(error, 'Unknown error');
-
-		if (
-			error instanceof DiscordAPIError &&
-			error.message === 'Unknown Message' &&
-			error.code === 10008
-		)
-			return;
-
-		if (error instanceof DiscordAPIError) console.error(error);
-
-		sendError(error, message);
-	}
-
-	async onReady() {
 		await setupDatabase();
-		console.log('Connected to database!');
-
-		await this.connectToLavalink();
+		await connectToLavalink();
+		loadCommands();
 
 		this.user?.setActivity({
 			name: `music! @ me`,
 			type: 'LISTENING',
 		});
 
-		this.loadCommands();
-
 		console.log(`Started at ${new Date().toString().substr(0, 24)}!`);
 	}
 
-	async onMessage(message: Message) {
+	private async onMessage(message: Message) {
 		try {
-			await this.handleCommand(message);
+			const {
+				id: messageId,
+				content,
+				author,
+				mentions,
+				channel,
+			} = message;
+
+			const isBot = author.bot;
+			if (isBot) return;
+
+			if (channel instanceof DMChannel) return;
+
+			const guild = await getGuildFromMessage(message);
+			const prefix = await this.guildResolver.prefix(guild.id);
+
+			const hasPrefix =
+				content.startsWith(prefix) ||
+				content.startsWith(prefix.concat(' '));
+			const mentionsBot = this.user
+				? mentions.has(this.user, {
+						ignoreEveryone: true,
+						ignoreRoles: true,
+				  })
+				: false;
+
+			if (!hasPrefix && !mentionsBot) return;
+
+			checkPermissions(guild);
+
+			const { id: guildId } = guild;
+
+			const prefixLength =
+				prefix.length + +content.startsWith(prefix.concat(' '));
+			const [name, ...args] = content.substr(prefixLength).split(' ');
+
+			const command = this.commands.find((c) =>
+				c.names.includes(mentionsBot ? 'help' : name)
+			);
+			if (!command) return;
+
+			if (command.needsArgs && args.length === 0)
+				throw new ArgumentError(command, prefix);
+
+			if (command.ownerOnly && author.id !== this.owner)
+				throw new OwnerError();
+
+			if (command.guildOnly && channel instanceof TextChannel === false)
+				throw new GuildOnlyError();
+
+			// TODO this do be kinda ugly tho
+			addToCommandQueue(guildId, messageId, {
+				channel,
+				command,
+				options: {
+					message,
+					args,
+					category: command.group,
+				},
+			});
 		} catch (error) {
-			this.handleError(message, error);
+			if (
+				error instanceof CustomError === false &&
+				error instanceof DiscordAPIError === false
+			)
+				return console.error(error, 'Unknown error');
+
+			if (
+				error instanceof DiscordAPIError &&
+				error.message === 'Unknown Message' &&
+				error.code === 10008
+			)
+				return;
+
+			if (error instanceof DiscordAPIError) console.error(error);
+
+			sendError(error, message);
 		}
 	}
 
-	async onVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
+	private async onVoiceStateUpdate(
+		oldState: VoiceState,
+		newState: VoiceState
+	) {
 		// TODO also do be kinda ugly
 		const guild = this.guilds.cache.get(oldState.guild.id);
 		if (!guild) return;
